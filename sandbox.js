73d358739a1d6d34982a38c467f945daa0fa91a4
Ayu Sudi <ayusudi.abc@gmail.com>
2020-10-21 11:12:16 +0700

    fitur mvp masigh bug

---
+++ sandbox.js
@@ -0,0 +1,15 @@
+let array = ['AI', 'Nodemailer', 'Translate']
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i])    
+}
+
+
+
+
+
+
+const myBio = {
+    name : 'Ayu'
+}
+
+console.log(myBio)
